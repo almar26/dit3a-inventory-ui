19a17724b30044a3622ab0f9f69fb4d84b70b9e6
Almar <gomezalmar26@gmail.com>
2024-03-11 13:15:30 +0800

    Added my Project

---
+++ pages/auth/login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="d-flex align-center justify-center"  style="height: 100vh;">
+    <v-card width="500" class="py-8">
+      <v-card-text>
+        <h1 class="text-center">Login</h1>
+        <v-form class="mx-5 mt-10">
+          <v-text-field label="Email Address" prepend-inner-icon="mdi-email" outlined></v-text-field>
+          <v-text-field label="Password" type="password" prepend-inner-icon="mdi-lock" outlined></v-text-field>
+          <v-btn block color="primary">Sign In</v-btn>
+          <v-btn block class="mt-2">Cancel</v-btn>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+    // layout: "auth"
+}
+</script>
